9c5bdf009b1b7772b8f42ed0896239500426e8c4
timishot <timidotcom58@gmail.com>
2023-07-19 00:12:35 +0100

    more on sql

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- script that lists the muber of records in score
-SELECT `score`, COUNT(*) AS `number` FROM `second_table` GROUP BY `score` ORDER BY `number` DESC;
+SELECT `score`, COUNT(`score`) AS `number` FROM `second_table` GROUP BY `score` ORDER BY `number` DESC;
