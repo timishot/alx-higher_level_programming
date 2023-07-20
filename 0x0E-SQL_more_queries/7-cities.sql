6ae1f48d5abe111504c4bb4be54bec57726a16f4
timishot <timidotcom58@gmail.com>
2023-07-21 00:14:10 +0100

    more on sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 
-USE `hbtn_0d_usa`
+USE `hbtn_0d_usa`;
 
 CREATE TABLE IF NOT EXISTS `cities`(
 	`id` INT UNIQUE AUTO_INCREMENT NOT NULL, 
