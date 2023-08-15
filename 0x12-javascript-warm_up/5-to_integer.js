db2d346c0798871a280f8035543f6c185d692b0e
timishot <timidotcom58@gmail.com>
2023-08-15 20:25:50 +0100

    learnt how to convert value to integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+
+const num  = parseInt(argv[2]);
+
+if (!isNaN(num)) {
+  console.log(`My number: ${num}`);
+} else {
+  console.log("Not a number");
+}
