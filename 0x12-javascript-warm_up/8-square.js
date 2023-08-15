c5bd4152b0fc61c2c8637a582ce6679e286f6cd2
timishot <timidotcom58@gmail.com>
2023-08-15 21:23:57 +0100

    more on javscript

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,10 +3,9 @@
 const { argv } = require('process');
 
 const num = parseInt(argv[2]);
-const number = parseInt(argv[2])
 
-if (argv.length <= 2) {
-  console.log('Missig size');
+if (isNaN(num)) {
+  console.log('Missing size');
 } else if (argv.length === 3) {
   for (let i = 0; i < num; i++) {
     console.log('X'.repeat(num));
