51165ca426be50692f2264c73fee90e30d682c5c
timishot <timidotcom58@gmail.com>
2023-08-15 20:29:00 +0100

    more conversion

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,10 +2,10 @@
 
 const { argv } = require('process');
 
-const num  = parseInt(argv[2]);
+const num = parseInt(argv[2]);
 
 if (!isNaN(num)) {
   console.log(`My number: ${num}`);
 } else {
-  console.log("Not a number");
+  console.log('Not a number');
 }
