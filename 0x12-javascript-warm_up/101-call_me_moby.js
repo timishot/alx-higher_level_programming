dec3a41c0b211df3b5d700f92514c6862f85dd8f
timishot <timidotcom58@gmail.com>
2023-08-16 23:47:02 +0100

    more on export

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-exports.callMeMoby = function (x, theFunction){
-	for (let i = 0; i < x; i++) {
-		theFunction();
-	}
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
 };
