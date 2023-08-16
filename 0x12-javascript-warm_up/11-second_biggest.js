4126983feee9130118ed5f9aeecd32d19960212f
timishot <timidotcom58@gmail.com>
2023-08-16 15:05:52 +0100

    object task

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,13 +5,13 @@ const { argv } = require('process');
 if (argv.length <= 2) {
   console.log(0);
 } else if (argv.length > 2) {
-  let temp = 0;
-  let before = 0;
-  for (let i = 0; i < argv.length - 2; i++) {
-    if (argv[2 + i] > temp) {
-      before = temp;
-      temp = argv[2 + i];
+  let largest = 0;
+  let secondLargest = 0;
+  for (let i = 2; i < argv.length; i++) {
+    if (argv[i] > largest) {
+      secondLargest = largest;
+      largest = argv[i];
     }
   }
-  console.log(before);
+  console.log(secondLargest);
 }
