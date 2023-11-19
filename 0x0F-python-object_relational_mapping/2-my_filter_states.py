3caf435cb2d073fec4881ef4ee2d71de753f0bcd
timishot <timidotcom58@gmail.com>
2023-11-19 17:56:39 +0100

    Create a scropt that take a argument to find a name in the database

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument """
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    try:
+        db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states WHERE name = %s", (sys.argv[4],))
+
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        db.close()
+    except MySQLdb.Error as e:
+        try:
+            print("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
+        except IndexError:
+            print("MySQL Error: %s" % str(e))
