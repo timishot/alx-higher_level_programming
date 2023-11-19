a9e759da1040dc01be1583d92239e71852acb749
timishot <timidotcom58@gmail.com>
2023-11-19 18:36:01 +0100

    Correct the code

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,9 +6,8 @@ import sys
 if __name__ == "__main__":
     try:
         # connect to the MySQL database
-        db =
-        MySQLdb.connect(host="localhost", user=sys.argv[1],
-                        passwd=sys.argv[2], db=sys.argv[3], port=3306)
+        db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                             passwd=sys.argv[2], db=sys.argv[3], port=3306)
         cur = db.cursor()
         cur.execute("SELECT * FROM states ORDER BY states.id ASC")
         rows = cur.fetchall()
