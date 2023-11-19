74d1572884e2617c9ea71cd16c79d4fba78dddc0
timishot <timidotcom58@gmail.com>
2023-11-19 21:08:46 +0100

    Finally break through

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" script that lists all cities from the database hbtn_0e_4_usa """
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT c.id, c.name, s.name FROM cities AS c INNER JOIN\
+            states AS s ON c.id = s.id ORDER BY c.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
