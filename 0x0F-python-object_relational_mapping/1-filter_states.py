8a0b0211b57fa69a5ebb1c7f01dcff6cede221d9
timishot <timidotcom58@gmail.com>
2023-11-19 18:56:11 +0100

    more correction

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-"""Script that lists all the states with a name starting with N from the date"""
+"""Script that lists all the states with a name
+starting with N from the date"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
     try:
         db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-        cur=db.cursor()
+                             passwd=sys.argv[2], db=sys.argv[3], port=3306)
+        cur = db.cursor()
         cur.execute("SELECT * FROM states WHERE left(name, 1) = 'N'")
-        rows=cur.fetchall()
+        rows = cur.fetchall()
         for row in rows:
             print(row)
         cur.close()
         db.close()
     except MySQLdb.Error as e:
         try:
-            #Print the MYSQL error details
+            """Print the MYSQL error details"""
             print("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
         except IndexError:
             print("MySQL Error: %s" % str(e))
