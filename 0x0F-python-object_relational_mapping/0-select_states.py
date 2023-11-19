f2826e3fc93c8e6f6066a7f0ec4b58beaca31fe7
timishot <timidotcom58@gmail.com>
2023-11-19 18:33:33 +0100

    Corrected the code

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,9 @@ import sys
 if __name__ == "__main__":
     try:
         # connect to the MySQL database
-        db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+        db =
+        MySQLdb.connect(host="localhost", user=sys.argv[1],
+                        passwd=sys.argv[2], db=sys.argv[3], port=3306)
         cur = db.cursor()
         cur.execute("SELECT * FROM states ORDER BY states.id ASC")
         rows = cur.fetchall()
@@ -14,10 +16,9 @@ if __name__ == "__main__":
             print(row)
         cur.close()
         db.close()
-    except  MySQLdb.Error as e:
+    except MySQLdb.Error as e:
         try:
             # Print the MySQL error details
             print("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
         except IndexError:
             print("MySQL Error: %s" % str(e))
-
