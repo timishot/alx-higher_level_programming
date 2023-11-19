1792b85986371b7a2866ec72f87c88accc669347
timishot <timidotcom58@gmail.com>
2023-11-19 20:19:05 +0100

    More changes

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Script that lists all the states with a name
-starting with N from the date"""
+""" script that takes in an argument and displays
+all values in the states table of
+hbtn_0e_0_usa where name matches the argument """
+
 import MySQLdb
 import sys
 
@@ -9,7 +11,8 @@ if __name__ == "__main__":
         db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                              passwd=sys.argv[2], db=sys.argv[3], port=3306)
         cur = db.cursor()
-        cur.execute("SELECT * FROM states WHERE left(name, 1) = 'N'")
+        cur.execute("SELECT * FROM states WHERE name = %s", (sys.argv[4],))
+
         rows = cur.fetchall()
         for row in rows:
             print(row)
@@ -17,7 +20,6 @@ if __name__ == "__main__":
         db.close()
     except MySQLdb.Error as e:
         try:
-            """Print the MYSQL error details"""
             print("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
         except IndexError:
             print("MySQL Error: %s" % str(e))
