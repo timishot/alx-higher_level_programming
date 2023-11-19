9d47327f03b530778375554272dc3966b21a0389
timishot <timidotcom58@gmail.com>
2023-11-19 19:20:21 +0100

    Corrected the spacing

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                              passwd=sys.argv[2], db=sys.argv[3], port=3306)
         cur = db.cursor()
-        cur.execute("SELECT * FROM states WHERE left(name, 1) = 'N'")
+        cur.execute("SELECT * FROM states WHERE left(name, 1) = 'N' ORDER BY id ASC")
         rows = cur.fetchall()
         for row in rows:
             print(row)
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-""" script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument """
+""" script that takes in an argument and displays
+all values in the states table of
+hbtn_0e_0_usa where name matches the argument """
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
     try:
-        db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+        db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                             passwd=sys.argv[2], db=sys.argv[3], port=3306)
         cur = db.cursor()
         cur.execute("SELECT * FROM states WHERE name = %s", (sys.argv[4],))
 
