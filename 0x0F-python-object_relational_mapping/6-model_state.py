c5cbdab5e77db85340665647d057c5049b918763
timishot <timidotcom58@gmail.com>
2023-11-20 10:42:37 +0100

    Update

---
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Start link class to table in database 
+"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}',
+        pool_pre_ping=True
+    )
