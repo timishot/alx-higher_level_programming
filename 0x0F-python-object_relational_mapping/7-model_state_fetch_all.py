14a36c3774bc34c385edfa338a0f714bbbaf2a4a
timishot <timidotcom58@gmail.com>
2023-11-20 22:25:59 +0100

    update

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,8 +8,7 @@ from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
     try:
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}\
-        '.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
         Base.metadata.create_all(engine)
         Session = sessionmaker()
         local_session = Session(bind=engine)
