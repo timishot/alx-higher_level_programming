eead0274a68b021d866a73df83b31065c2cc1a1e
timishot <timidotcom58@gmail.com>
2023-11-20 22:07:15 +0100

    make a query to print all the all the data in the database

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    try:
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+        Base.metadata.create_all(engine)
+        Session=sessionmaker()
+        local_session=Session(bind=engine)
+        users = local_session.query(State).all()
+        for index, value in enumerate(users, start=1):
+            print(f"{index}: {value.name}")
+    except Exception as e:
+        print(f"Error: {e}")
