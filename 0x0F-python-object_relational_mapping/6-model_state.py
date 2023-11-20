ae986bf85c18bd0c4db877e8bf2f6784f90f59df
timishot <timidotcom58@gmail.com>
2023-11-20 21:26:01 +0100

    More on sql

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -7,9 +7,9 @@ from model_state import Base, State
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    try:
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+        Base.metadata.create_all(engine)
+    except Exception as e:
+        print(f"Error: {e}")
 
-    engine = create_engine(
-        f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}',
-        pool_pre_ping=True
-    )
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,11 +2,10 @@
 """
 Contains State class and Base, an instance of declarative_base()
 """
+
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
-
-mymetadata = MetaData()
-Base = declarative_base(metadata=mymetadata)
+Base = declarative_base()
 
 
 class State(Base):
@@ -14,5 +13,6 @@ class State(Base):
     Class with id and name attributes of each state
     """
     __tablename__ = 'states'
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
+
