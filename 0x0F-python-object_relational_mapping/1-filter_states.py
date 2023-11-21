f89086f559c3f9b0fb9d12482c87490929b92b29
timishot <timidotcom58@gmail.com>
2023-11-21 15:37:21 +0100

    More update

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,11 +7,11 @@ import sys
 if __name__ == "__main__":
     try:
         if len(sys.argv) != 4:
-            print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+            print("Usage: {} <username> <password> \
+                  <database>".format(sys.argv[0]))
             sys.exit(1)
         db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                              passwd=sys.argv[2], db=sys.argv[3], port=3306)
-        
         cur = db.cursor()
         cur.execute("SELECT * FROM states WHERE left(name, 1)\
                      = 'N' ORDER BY states.id ASC")
