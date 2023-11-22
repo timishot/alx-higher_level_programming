f805c2588f79d905656e8124971ae33c2daaf944
timishot <timidotcom58@gmail.com>
2023-11-22 13:46:51 +0100

    more on sql

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from relationship_city import City
+from relationship_state import State, Base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    try:
+        us = sys.argv[1]
+        pwd = sys.argv[2]
+        db = sys.argv[3]
+
+        engine = \
+            create_engine(f"mysql+mysqldb://{us}:{pwd}@localhost/{db}")
+        Base.metadata.create_all(engine)
+        Session = sessionmaker()
+        local_session = Session(bind=engine)
+        cities = local_session.query(City).order_by(City.id).all()
+        for city in cities:
+             state=city.states
+             print(f"{city.id}: {city.name} -> {state.name}")
+        local_session.commit()
+    except Exception as e:
+        print(f"Error: {e}")
