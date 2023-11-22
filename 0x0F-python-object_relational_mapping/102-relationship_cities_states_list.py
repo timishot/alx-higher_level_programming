849f5c74187908eee956d172f28d7cc6904ab5d3
timishot <timidotcom58@gmail.com>
2023-11-22 13:49:44 +0100

    more on sql

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -20,8 +20,8 @@ if __name__ == "__main__":
         local_session = Session(bind=engine)
         cities = local_session.query(City).order_by(City.id).all()
         for city in cities:
-             state=city.states
-             print(f"{city.id}: {city.name} -> {state.name}")
+            state = city.states
+            print(f"{city.id}: {city.name} -> {state.name}")
         local_session.commit()
     except Exception as e:
         print(f"Error: {e}")
