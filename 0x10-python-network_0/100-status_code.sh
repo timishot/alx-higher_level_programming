a9111ad7b8314e49da32c51c0bc480b40ead75d0
timishot <timidotcom58@gmail.com>
2023-12-01 01:45:02 +0100

    Finally

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -sI "$1" | grep  "HTTP/1.1" | awk '{print $2}'
+# Sends a GET request to a given URL and display the response status code.
+curl -s -o /dev/null -w "%{http_code}" "$1"
