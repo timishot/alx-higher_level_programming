45089656e37bbbea646a1af25c774f76ee91444c
timishot <timidotcom58@gmail.com>
2023-12-01 20:32:46 +0100

    work

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL and displays
-        the value of the X-Request-Id
-        variable found in the header of the response."""
+"""Python script that takes in a URL"""
+
 import urllib.request
 import sys
 
