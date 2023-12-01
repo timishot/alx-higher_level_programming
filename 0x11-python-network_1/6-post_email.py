7cc2a7485889b5bbec44d7bb50f1e14ce041fcdb
timishot <timidotcom58@gmail.com>
2023-12-01 14:31:22 -0800

    add

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
 	url = sys.argv[1]
 	email = sys.argv[2]
 	emails = {'email': email}
-	r = requests.post(url, emails=emails)
+	r = requests.post(url, email=emails)
 	print(r.text)
