0022975658880a956e55da414180917e5eb0b9d2
timishot <timidotcom58@gmail.com>
2023-12-01 19:33:56 +0100

    resolve

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
         with urllib.request.urlopen(req) as response:
             print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
-        print(f"Error code: {e}")
+        print(f"{e}")
