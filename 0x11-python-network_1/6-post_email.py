53da19f403e9324854243b8af2ce525c7f1b1f46
timishot <timidotcom58@gmail.com>
2023-12-01 14:38:22 -0800

    more

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,16 +4,10 @@ import requests
 import sys
 
 if __name__ == "__main__":
-	if len(sys.argv) != 3:
-		exit(1)
-	url = sys.argv[1]
-	email = sys.argv[2]
-<<<<<<< HEAD
-	emails = {'email': email}
-	r = requests.post(url, email=emails)
-	print(r.text)
-=======
-	payload = {'email': email}
-	r = requests.post(url, data=payload)
-	print(r.text)
->>>>>>> 15d0a39ad9f4393c35d6e7d36091a633691c2f35
+    if len(sys.argv) != 3:
+        exit(1)
+    url = sys.argv[1]
+    email = sys.argv[2]
+    payload = {'email': email}
+    r = requests.post(url, data=payload)
+    print(r.text)
