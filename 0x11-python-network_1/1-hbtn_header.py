1c0326d4c52462d72e5f3f62101c9deb2a55cf18
timishot <timidotcom58@gmail.com>
2023-12-01 20:22:29 +0100

    resolve

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,7 +7,6 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-
     request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as response:
         x_id = response.getheader('X-Request-Id', 'Not Present')
