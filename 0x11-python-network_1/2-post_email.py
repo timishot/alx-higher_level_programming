e48b51f293e4b6b785dadf5573b34356c2e5ba61
timishot <timidotcom58@gmail.com>
2023-12-01 10:17:21 -0800

    pull

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,7 +3,7 @@ import urllib.request
 import sys
 import urllib.parse
 """sends a POST request to the passed URL """
-if __name__ == "__main__":
+"""if __name__ == "__main__":
     url = sys.argv[1]
     data = {'email': sys.argv[2]}
 
@@ -11,4 +11,24 @@ if __name__ == "__main__":
     req = urllib.request.Request(url, data=url_value, method='POST')
     with urllib.request.urlopen(req) as response:
         the_page = response.read().decode('utf-8')
-        print(the_page)
+        print(the_page)"""
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    # Encode the email data
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+
+    try:
+        # Create a POST request with the specified data
+        request = urllib.request.Request(url, data=data, method='POST')
+
+        with urllib.request.urlopen(request) as response:
+            # Decode and print the body of the response
+            body = response.read().decode('utf-8')
+            print(body)
+    except urllib.error.URLError as e:
+        print(f"Error accessing the URL: {e}")
+        sys.exit(1)
+
