e372714787cf0920bcb2a9669a066bc639f06e49
timishot <timidotcom58@gmail.com>
2023-12-01 20:31:12 +0100

    solve

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,9 +6,10 @@ import urllib.request
 import sys
 
 if __name__ == "__main__":
-    if (len(sys.argv) != 2):
+    if len(sys.argv) != 2:
         print("Usage: {} <URL>".format(sys.argv[0]))
         sys.exit(1)
+
     url = sys.argv[1]
     with urllib.request.urlopen(url) as response:
         x_id = response.getheader('X-Request-Id', 'Not Present')
