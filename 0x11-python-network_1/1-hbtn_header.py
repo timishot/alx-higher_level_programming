56a50bf5b91bcff46cbea075475956755e8a1cc1
timishot <timidotcom58@gmail.com>
2023-12-01 20:27:13 +0100

    push

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,6 +6,9 @@ import urllib.request
 import sys
 
 if __name__ == "__main__":
+    if (len(sys.argv) != 2):
+        print("Usage: {} <URL>".format(sys.argv[0]))
+        sys.exit(1)
     url = sys.argv[1]
     request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as response:
