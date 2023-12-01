0e220b168bb5e8592ec40f146805e82c372b22f5
timishot <timidotcom58@gmail.com>
2023-12-01 16:09:24 +0100

    script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-import urllib.request
+
 """print(f"    - type: {type(content)}")"""
-req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-with urllib.request.urlopen(req) as response:
-    content = response.read()
-print("Body response:")
-print(f"    - type: {type(content)}")
-print(f"    - content: {content}")
-print(f"    - utf8 content: {content.decode('utf-8')}")
+if __name__ == '__main__':
+    import urllib.request
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        content = response.read()
+    print("Body response:")
+    print(f"    - type: {type(content)}")
+    print(f"    - content: {content}")
+    print(f"    - utf8 content: {content.decode('utf-8')}")
