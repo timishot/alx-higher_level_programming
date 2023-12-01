47cf1990989cc6255f6b54a4541aca4baff2dc02
timishot <timidotcom58@gmail.com>
2023-12-01 18:46:28 +0100

    push

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import urllib.request
-import sys
 """Python script that takes in a URL, sends a request to the URL and displays\
         the value of the X-Request-Id \
         variable found in the header of the response."""
+import urllib.request
+import sys
+
 if __name__ == '__main__':
     url = sys.argv[1]
 
