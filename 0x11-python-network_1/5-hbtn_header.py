eed1272dfb4a1d389447c70d38b6149bdde74afa
timishot <timidotcom58@gmail.com>
2023-12-01 20:46:19 +0100

    push

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,5 +7,8 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
-    x_requests_id = response.headers['X-Request-Id']
-    print(x_requests_id)
+    if 'X-Request-Id' in response.headers:
+        x_request_id = response.headers['X-Request-Id']
+        print(x_request_id)
+    else:
+        print("X-Request-Id header not found in the response.")
