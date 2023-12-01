28d449a66714038a334acffaac9e846458ea7aeb
timishot <timidotcom58@gmail.com>
2023-12-01 20:17:13 +0100

    resolve x_id

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""X-Request-Id in the response header"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    x_requests_id = response.headers['X-Request-Id']
+    print(x_requests_id)
