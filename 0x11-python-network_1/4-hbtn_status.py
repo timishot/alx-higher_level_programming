0965f7529e1fe6ebf31cf29dcc3c7322bedbdf20
timishot <timidotcom58@gmail.com>
2023-12-01 20:08:40 +0100

    resolve

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,7 +3,7 @@
 import requests
 
 if __name__ == "__main__":
-	r = requests.get('https://alx-intranet.hbtn.io/status')
-	print(f"Body response:")
-	print(f"\t- type: {type(r.text)}")
-	print(f"\t- content: {r.text}")
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print(f"Body response:")
+    print(f"\t- type: {type(r.text)}")
+    print(f"\t- content: {r.text}")
