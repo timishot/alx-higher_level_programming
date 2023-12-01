4c62f42260a8b1e682058aec02b3ac2f0a4d0b3e
timishot <timidotcom58@gmail.com>
2023-12-01 18:49:25 +0100

    push

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL and displays\
-        the value of the X-Request-Id \
+"""Python script that takes in a URL, sends a request to the URL and displays
+        the value of the X-Request-Id
         variable found in the header of the response."""
 import urllib.request
 import sys
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     url = sys.argv[1]
 
     with urllib.request.urlopen(url) as response:
