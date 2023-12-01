916e95480508fad9ab26fe473d1c574a9742f1ff
timishot <timidotcom58@gmail.com>
2023-12-01 23:20:40 +0100

    email post

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL"""
+import requests
+import sys
+
+if __name__ == "__main__":
+	if len(sys.argv) != 3:
+		exit(1)
+	url = sys.argv[1]
+	email = sys.argv[2]
+	emails = {'email': email}
+	r = requests.post(url, emails=emails)
+	print(r.text)
