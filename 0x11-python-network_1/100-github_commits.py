f03128db3e649549357e1d248b3bb4b1ac181ba5
timishot <timidotcom58@gmail.com>
2023-12-01 16:06:35 -0800

    push

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" python script that takes 2 arguments """
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("complete")
+        exit(1)
+    repository_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+    r = requests.get(url)
+    if r.status_code == 200:
+        user_data = r.json()
+        for user in user_data:
+            print(f"{user['sha']}: {user['commit']['author']['name']}")
