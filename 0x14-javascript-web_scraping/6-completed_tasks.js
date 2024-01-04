19efc0b0d18445ca720d02d8db26ed68beb7e8bd
Timishot <timidotcom58@gmail.com>
2024-01-04 08:57:23 +0100

    fix

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,29 +1,26 @@
 #!/usr/bin/node
+
 const request = require('request');
+const url = process.argv[2];
 
-const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
-
-request(apiUrl, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const completed = {};
     const tasks = JSON.parse(body);
-
-    // Create an object to store the count of completed tasks for each user
-    const completedTasksByUser = {};
-
-    // Iterate through tasks and count completed tasks for each user
-    tasks.forEach((task) => {
-      if (task.completed) {
-        if (completedTasksByUser[task.userId]) {
-          completedTasksByUser[task.userId]++;
+    for (const i in tasks) {
+      const task = tasks[i];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
         } else {
-          completedTasksByUser[task.userId] = 1;
+          completed[task.userId]++;
         }
       }
-    });
-
-    // Output the results
-    console.log(completedTasksByUser);
+    }
+    console.log(completed);
   } else {
-    console.error(`Error: ${response && response.statusCode}`);
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
 });
