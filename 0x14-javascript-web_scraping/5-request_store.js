f8a5eaa81eadda8eca5967a7898faa5725fbcf06
Timishot <timidotcom58@gmail.com>
2024-01-04 08:58:57 +0100

    fix

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,20 +1,4 @@
 #!/usr/bin/node
-const request = require('request');
 const fs = require('fs');
-
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-request(url, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
-    fs.writeFile(filePath, body, 'utf-8', function (err) {
-      if (err) {
-        console.error('Error writing to file:', err);
-      } else {
-        console.log(`Body content successfully written to ${filePath}`);
-      }
-    });
-  } else {
-    console.error(`Error: ${response && response.statusCode}`);
-  }
-});
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
