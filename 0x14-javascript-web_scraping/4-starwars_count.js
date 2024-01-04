7f30c32bfd2940efcfad125bc10992b0e3911df6
Timishot <timidotcom58@gmail.com>
2024-01-04 09:01:35 +0100

    fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,20 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-
-const apiUrl = process.argv[2];
-const characterId = 18;
-
-request(apiUrl, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
-    const filmsData = JSON.parse(body).results;
-    const count = filmsData.reduce((acc, film) => {
-      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
-        return acc + 1;
-      }
-      return acc;
-    }, 0);
-    console.log(count);
-  } else {
-    console.error(`Error: ${response && response.statusCode}`);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
